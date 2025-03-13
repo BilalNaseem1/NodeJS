29a458638176a102f7891bf532111f20df823981
BilalNaseemChikoo <bilalnaseem@brandver.se>
2025-03-13 10:27:55 +0500

    map done

--- 01_JS_Refresher/08_Arrays.js
+++ 01_JS_Refresher/08_Arrays.js
@@ -9,7 +9,7 @@ console.log(hobbies[1]);
 console.log(hobbies[2]);
 console.log(hobbies[3]);
 
-//looping through an array
+// looping through an array
 for (let i = 0; i < hobbies.length; i++) {
     console.log(hobbies[i]);
 }
